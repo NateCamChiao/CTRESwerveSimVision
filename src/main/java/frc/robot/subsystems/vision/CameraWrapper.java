2e960d2e8ee96d44bb35639b6c054c89e8ee18b2
Nate <142466177+NateCamChiao@users.noreply.github.com>
2026-07-10 23:23:05 -0700

    Add update method to visionMeasurment and getters. Add cached vision measurement to hold last measrument and getPositionStandardDeviations to calculate stdDevs.

--- src/main/java/frc/robot/subsystems/vision/CameraWrapper.java
+++ src/main/java/frc/robot/subsystems/vision/CameraWrapper.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonUtils;
 import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
 import org.photonvision.targeting.PhotonPipelineResult;
@@ -12,6 +13,7 @@ import org.photonvision.targeting.PhotonPipelineResult;
 import dev.doglog.DogLog;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
@@ -25,10 +27,30 @@ public class CameraWrapper {
     private final SimCameraProperties cameraProp = new SimCameraProperties();
     public final Transform3d robotToCamera;
     private VisionMeasurement mutableMeasurement = new VisionMeasurement();
-
+    private Optional<VisionMeasurement> cachedLastMeasurement = Optional.empty();
     public class VisionMeasurement {
-        public EstimatedRobotPose measurementInfo;
-        public Matrix<N3, N1> standardDeviations;
+        private EstimatedRobotPose measurementInfo; // includes timestamp and pose
+        private final Matrix<N3, N1> standardDeviations = VecBuilder.fill(9, 9, 1000);
+
+        public void updateMeasurement(EstimatedRobotPose newEstimate, double translationStdDevs, double rotationStdDevs){
+            this.measurementInfo = newEstimate;
+            // edit standardDeviation object
+            if(translationStdDevs <= 0)
+                translationStdDevs = 0.1;
+            if(rotationStdDevs <= 0)
+                rotationStdDevs = 0.1;
+            this.standardDeviations.set(0,0,translationStdDevs); // x stdDevs
+            this.standardDeviations.set(1,0,translationStdDevs); // y stdDevs
+            this.standardDeviations.set(2,0,rotationStdDevs);
+        }
+
+        public EstimatedRobotPose getMeasurementInfo(){
+            return this.measurementInfo;
+        }
+        
+        public Matrix<N3, N1> getStandardDeviations(){
+            return this.standardDeviations;
+        }
     }
 
     public CameraWrapper(String cameraName, AprilTagFieldLayout tagLayout, Transform3d robotToCamera,
@@ -39,33 +61,84 @@ public class CameraWrapper {
         this.robotToCamera = robotToCamera;
     }
 
-    public Optional<VisionMeasurement> getLatestPose() {
+    // get the latest unread measurement (for odometry) 
+    // should be called periodically to update last measurement
+    public Optional<VisionMeasurement> getLatestUnreadMeasurement() {
         Optional<VisionMeasurement> latestPose = Optional.empty();
-        var results = camera.getAllUnreadResults();
-        if (results.size() >= 2) {
-            DogLog.log("0th index",
-                    results.get(0).getTimestampSeconds() > results.get(results.size() - 1).getTimestampSeconds());
+        var results = camera.getAllUnreadResults(); // earliest to latest results
+        // if (results.size() >= 2) {
+            // DogLog.log("0th index","" +
+            // results.get(0).getTimestampSeconds() + ", " + results.get(results.size() -
+            // 1).getTimestampSeconds());
+        // }
 
+        if (results.size() <= 0) {
+            return Optional.empty();
         }
-        double latestTimestamp = 0;
-        for (PhotonPipelineResult result : results) {
-            // check targets before anything else to optimize loop
-            if (result.hasTargets() && result.getTimestampSeconds() > latestTimestamp) {
-                latestTimestamp = result.getTimestampSeconds();
+        PhotonPipelineResult latestResult = results.get(results.size() - 1); // grab the latest result (last in list)
+        // check if has targets before anything else to optimize loop
+        if (!latestResult.hasTargets()) {
+            return Optional.empty();
+        }
 
-                if (result.getTargets().size() == 1) {
-                    latestPose = Optional.of(mutableMeasurement);
-                    mutableMeasurement.measurementInfo = poseEstimator.estimatePnpDistanceTrigSolvePose(result).get();
-                    // set rotation standard deviation very low to avoid feedback loop (since we use
-                    // gyro to help solve)
-                } else if (latestPose.isEmpty() || result.getTargets().size() >= 2) {
-                    latestPose = Optional.of(mutableMeasurement);
-                    mutableMeasurement.measurementInfo = poseEstimator.estimateCoprocMultiTagPose(result).get();
-                }
+        if (latestResult.getTargets().size() == 1 && latestResult.getBestTarget().getPoseAmbiguity() < 0.2) {
+            // estimatedPose should get allocated to stack bc it doesn't escape the scope
+            EstimatedRobotPose estimatedPose = poseEstimator.estimatePnpDistanceTrigSolvePose(latestResult).get(); 
+
+            this.mutableMeasurement.updateMeasurement(estimatedPose, 1, 1000);
+            return Optional.of(this.mutableMeasurement);
+            // set rotation standard deviation very low to avoid feedback loop (since we use
+            // gyro to help solve)
+        } else if (latestResult.getTargets().size() >= 2 && latestResult.getMultiTagResult().isPresent()) {
+            EstimatedRobotPose estimatedPose = poseEstimator.estimateCoprocMultiTagPose(latestResult).get();
+
+            this.mutableMeasurement.updateMeasurement(estimatedPose, 1, 1);
+            return Optional.of(mutableMeasurement);
+        }
+        
+        // update cache when new measurement is taken
+        if(latestPose.isPresent())
+            cachedLastMeasurement = latestPose;
+        
+        return latestPose;
+    }
+
+    public double getPositionStandardDeviations(EstimatedRobotPose robotPose, PhotonPipelineResult result){
+        // Pose present. Start running Heuristic
+        int numTags = 0;
+        double avgDist = 0;
+
+        // Precalculation - see how many tags we found, and calculate an average-distance metric
+        for (var tgt : result.targets) {
+            var tagPose = poseEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
+            if (tagPose.isEmpty()) continue;
+            numTags++;
+            avgDist +=
+                    tagPose
+                            .get()
+                            .toPose2d()
+                            .getTranslation()
+                            .getDistance(robotPose.estimatedPose.toPose2d().getTranslation());
+        }
+
+        if (numTags == 0) {
+            // No tags visible. Default to single-tag std devs
+            return 9999; 
+        } else {
+            // One or more tags visible, run the full heuristic.
+            avgDist /= numTags;
+            // Increase std devs based on (average) distance
+            if (numTags == 1 && avgDist > 4)
+                return 9999;
+            else {
+                return 1 + (avgDist * avgDist / 30);
             }
         }
-        return latestPose;
+    }
 
+    // gets last measurement
+    public Optional<VisionMeasurement> getLastMeasurement(){
+        return this.cachedLastMeasurement;
     }
 
     public void setHeading(Rotation2d rotation, double timestamp) {
@@ -73,6 +146,13 @@ public class CameraWrapper {
     }
 
     public void setCameraDisable(boolean shouldDisable) {
-        this.camera.setDriverMode(shouldDisable);
+        if(shouldDisable){
+            this.camera.setFPSLimit(1);
+            this.camera.setPipelineIndex(1);
+        }
+        else{
+            this.camera.setFPSLimit(-1); // set to unlimited frames
+            this.camera.setPipelineIndex(0);
+        }
     }
 }
